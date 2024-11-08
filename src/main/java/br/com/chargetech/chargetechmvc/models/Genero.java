5122a3baa8cf6147b60d22ea1b04d272ba1ec2e6
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-08 20:25:11 -0300

    feat: Adição do atributo label em Genênero

--- src/main/java/br/com/chargetech/chargetechmvc/models/Genero.java
+++ src/main/java/br/com/chargetech/chargetechmvc/models/Genero.java
@@ -19,8 +19,11 @@ public class Genero {
     @Column(name = "ID_GENERO")
     private Long id;
 
-    @Column(name = "NM_GENERO", length = 30, nullable = false)
+    @Column(name = "NM_GENERO", length = 30, nullable = false, unique = true)
     private String nome;
+
+    @Column(name = "DS_LABEL", length = 100, nullable = false)
+    private String label;
 
     @Column(name = "DS_GENERO", length = 200, nullable = false)
     private String descricao;
