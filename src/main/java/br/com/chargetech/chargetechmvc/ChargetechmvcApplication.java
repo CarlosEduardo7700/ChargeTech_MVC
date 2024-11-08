7483bc1f78fc6efdcf04373c05df6f005a7519bc
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-07 22:47:35 -0300

    Enviando o projeto para GitHub

---
+++ src/main/java/br/com/chargetech/chargetechmvc/ChargetechmvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.chargetech.chargetechmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargetechmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargetechmvcApplication.class, args);
+	}
+
+}
