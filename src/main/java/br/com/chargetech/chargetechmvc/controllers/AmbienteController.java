8c3bdf860b514bba06d9b502b5ee25b2eadd955a
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-08 22:38:21 -0300

    feat: Criação da funcionalidade de Cadastrar Ambiente

--- src/main/java/br/com/chargetech/chargetechmvc/controllers/AmbienteController.java
+++ src/main/java/br/com/chargetech/chargetechmvc/controllers/AmbienteController.java
@@ -1,18 +1,46 @@
 package br.com.chargetech.chargetechmvc.controllers;
 
 import br.com.chargetech.chargetechmvc.dtos.ambiente.CadastroDeAmbienteDto;
+import br.com.chargetech.chargetechmvc.dtos.usuario.CadastroDeUsuarioDto;
+import br.com.chargetech.chargetechmvc.models.Ambiente;
+import br.com.chargetech.chargetechmvc.repositories.AmbienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
 
 @Controller
 @RequestMapping("ambiente")
 public class AmbienteController {
 
+    @Autowired
+    private AmbienteRepository ambienteRepository;
+
     @GetMapping("cadastrar")
     public String exibirCadastroDeAmbiente(CadastroDeAmbienteDto dto, Model model) {
         model.addAttribute("ambiente", dto);
         return "ambiente/form-cadastrar";
+    }
+
+    @PostMapping("cadastrar")
+    public String cadastrarAmbiente(@Valid @ModelAttribute("ambiente") CadastroDeAmbienteDto dto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "ambiente/form-cadastrar";
+        }
+
+        Ambiente ambiente = new Ambiente();
+        ambiente.setNome(dto.nome());
+        ambiente.setDescricao(dto.descricao());
+        ambiente.setLabel(dto.nome().toUpperCase());
+
+        ambienteRepository.save(ambiente);
+        return "redirect:/ambiente/cadastrar";
     }
 }
