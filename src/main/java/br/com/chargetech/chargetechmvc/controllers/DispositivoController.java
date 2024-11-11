91b5a13106114edfb17f51efbdc295f1d2adc558
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-11 16:43:22 -0300

    feat: Criação da funcionalidade de Cadastro de Dispositivos

--- src/main/java/br/com/chargetech/chargetechmvc/controllers/DispositivoController.java
+++ src/main/java/br/com/chargetech/chargetechmvc/controllers/DispositivoController.java
@@ -1,25 +1,62 @@
 package br.com.chargetech.chargetechmvc.controllers;
 
+import br.com.chargetech.chargetechmvc.dtos.ambiente.CadastroDeAmbienteDto;
 import br.com.chargetech.chargetechmvc.dtos.dispositivo.CadastroDeDispositivoDto;
+import br.com.chargetech.chargetechmvc.models.Ambiente;
+import br.com.chargetech.chargetechmvc.models.Dispositivo;
 import br.com.chargetech.chargetechmvc.repositories.AmbienteRepository;
+import br.com.chargetech.chargetechmvc.repositories.DispositivoRepository;
+import br.com.chargetech.chargetechmvc.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("dispositivo")
 public class DispositivoController {
 
     @Autowired
+    private DispositivoRepository dispositivoRepository;
+
+    @Autowired
     private AmbienteRepository ambienteRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @GetMapping("cadastrar")
     public String exibirFormularioCadastrar(CadastroDeDispositivoDto dto, Model model) {
         model.addAttribute("dispositivo", dto);
         model.addAttribute("ambientes", ambienteRepository.findAll());
         return "dispositivo/form-cadastrar";
+    }
+
+    @PostMapping("cadastrar")
+    public String cadastrarDispositivo(@Valid @ModelAttribute("dispositivo") CadastroDeDispositivoDto dto, BindingResult result, Model model, Principal principal, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            model.addAttribute("ambientes", ambienteRepository.findAll());
+            return "dispositivo/form-cadastrar";
+        }
+
+        Dispositivo dispositivo = new Dispositivo();
+        dispositivo.setNome(dto.nome());
+        dispositivo.setConsumoMedio(dto.consumoMedio());
+        dispositivo.setAmbiente(ambienteRepository.findByNome(dto.ambiente()));
+        dispositivo.setUsuario(usuarioRepository.findByEmail(principal.getName()));
+        dispositivo.setStatus("LIGADO");
+
+        dispositivoRepository.save(dispositivo);
+        redirectAttributes.addFlashAttribute("mensagem", "Dispositivo cadastrado!");
+        return "redirect:/dispositivo/cadastrar";
     }
 
 }
--- src/main/java/br/com/chargetech/chargetechmvc/repositories/AmbienteRepository.java
+++ src/main/java/br/com/chargetech/chargetechmvc/repositories/AmbienteRepository.java
@@ -4,4 +4,5 @@ import br.com.chargetech.chargetechmvc.models.Ambiente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AmbienteRepository extends JpaRepository<Ambiente, Long> {
+    Ambiente findByNome(String nome);
 }
