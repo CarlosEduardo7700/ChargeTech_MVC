1788561d8933cdeefb9418a8948cbaebfaee3229
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-19 00:13:17 -0300

    feat: Criação da funcionaliade para simulação dos log de consumo energético

--- src/main/java/br/com/chargetech/chargetechmvc/controllers/ConsumoEnergeticoController.java
+++ src/main/java/br/com/chargetech/chargetechmvc/controllers/ConsumoEnergeticoController.java
@@ -1,15 +1,34 @@
 package br.com.chargetech.chargetechmvc.controllers;
 
+import br.com.chargetech.chargetechmvc.dtos.dispositivo.CadastroDeDispositivoDto;
 import br.com.chargetech.chargetechmvc.dtos.dispositivo.ListagemDosDispositivosDto;
 import br.com.chargetech.chargetechmvc.models.ConsumoEnergetico;
+import br.com.chargetech.chargetechmvc.models.Dispositivo;
 import br.com.chargetech.chargetechmvc.repositories.ConsumoEnergeticoRepository;
+import br.com.chargetech.chargetechmvc.repositories.DispositivoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("consumo-energetico")
@@ -20,7 +39,10 @@ public class ConsumoEnergeticoController {
 
     @GetMapping
     public String exibirConsumoEnergetico(Model model) {
-        List<ConsumoEnergetico> consumosEnergeticos = consumoEnergeticoRepository.findAll();
+        List<ConsumoEnergetico> consumosEnergeticos = consumoEnergeticoRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(ConsumoEnergetico::getDataDeRegistro).reversed()) // Ordena por nome
+                .collect(Collectors.toList());
         model.addAttribute("consumosEnergeticos", consumosEnergeticos);
         return "consumo-nergetico/list-consumos";
     }
