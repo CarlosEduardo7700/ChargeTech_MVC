d27d08430c1e61f797683c15651ae6358625bf34
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-19 22:29:44 -0300

    feat: Paginação na lista de consumos de energético

--- src/main/java/br/com/chargetech/chargetechmvc/controllers/ConsumoEnergeticoController.java
+++ src/main/java/br/com/chargetech/chargetechmvc/controllers/ConsumoEnergeticoController.java
@@ -8,14 +8,15 @@ import br.com.chargetech.chargetechmvc.repositories.ConsumoEnergeticoRepository;
 import br.com.chargetech.chargetechmvc.repositories.DispositivoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
@@ -38,12 +39,20 @@ public class ConsumoEnergeticoController {
     private ConsumoEnergeticoRepository consumoEnergeticoRepository;
 
     @GetMapping
-    public String exibirConsumoEnergetico(Model model) {
-        List<ConsumoEnergetico> consumosEnergeticos = consumoEnergeticoRepository.findAll()
-                .stream()
-                .sorted(Comparator.comparing(ConsumoEnergetico::getDataDeRegistro).reversed()) // Ordena por nome
-                .collect(Collectors.toList());
-        model.addAttribute("consumosEnergeticos", consumosEnergeticos);
+    public String exibirConsumoEnergetico(
+            @RequestParam(defaultValue = "0") int pagina,
+            @RequestParam(defaultValue = "20") int tamanho,
+            Model model) {
+
+        Pageable pageable = PageRequest.of(pagina, tamanho, Sort.by("dataDeRegistro").descending());
+        Page<ConsumoEnergetico> consumosEnergeticosTeste = consumoEnergeticoRepository.findAll(pageable);
+
+
+        model.addAttribute("consumosEnergeticos", consumosEnergeticosTeste.getContent());
+        model.addAttribute("paginaAtual", pagina);
+        model.addAttribute("totalPaginas", consumosEnergeticosTeste.getTotalPages());
+        model.addAttribute("tamanhoPagina", tamanho);
+
         return "consumo-nergetico/list-consumos";
     }
 }
